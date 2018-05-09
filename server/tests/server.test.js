f31a6a026ea9fc75d0a17c8776ab8ce2cb4db682
Steven <codebusiness2014@gmail.com>
2018-05-09 10:16:58 -0400

    Working on a bug where delete test producing a 'property _id is undefined'

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -102,3 +102,41 @@ describe('GET /todos/:id', (req, res) => {
       .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('Should remove a todo', done => {
+    let hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect( res => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end( (err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId).then( todo => {
+          expect(todo).toNotExist();
+          done();
+        }).catch( e => done(e));
+      });
+  });
+
+  it('Should return 404 if todo not found', done => {
+    let hexID = new ObjectID().toHexString();
+    request(app)
+      .delete(`/todos/${hexID}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('Should return 404 if object id is invalid', done => {
+    request(app)
+      .delete(`/todos/123abc`)
+      .expect(404)
+      .end(done);
+  });
+});
