4a1b41ce4cff73a9491f645e5622493891c1b081
Steven <codebusiness2014@gmail.com>
2018-05-18 18:16:09 -0400

    Refactoring test file. Created a seed file

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,21 +4,9 @@ const { ObjectID }  = require('mongodb');
 
 const { app }       = require('./../server');
 const { Todo }      = require('./../models/todo');
+const { todos, populateTodos } = require('./seed/seed');
 
-const todos = [{
-  _id: new ObjectID(),
-  'text': 'First test todo'
-}, {
-  _id: new ObjectID(),
-  'text': 'Second test todo'
-}];
-
-beforeEach( done => {
-  //Remove all before test runs.
-  Todo.remove({}).then( () => {
-    return Todo.insertMany(todos);
-  }).then( () => done());
-});
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
   it('Should create a new todo', done => {
